cd05ee784e364e20da810c195e51f86a153f0d99
Micheal Bailey <mbaileyfuturist@gmail.com>
2022-03-17 02:05:17 -0400

    Implemented log4j to log information to a log file whenever a CRUD operation is performed on the Car and Model tables.

--- src/main/java/com/revature/dao/CarDao.java
+++ src/main/java/com/revature/dao/CarDao.java
@@ -2,15 +2,18 @@ package com.revature.dao;
 
 import java.util.ArrayList;
 
+import org.apache.log4j.Logger;
+
 import com.revature.models.Car;
 import com.revature.models.Country;
 import com.revature.models.Manufacturer;
-import com.revature.models.Model;
 import com.revature.orm.ORM;
 
 public class CarDao {
 	
 	private ORM orm = new ORM();
+	private static Logger logger = Logger.getLogger(CarDao.class);
+
 	
 	public void initializeParentTables(ArrayList<Country> countries, ArrayList<Manufacturer> manufacturers) {
 		
@@ -21,12 +24,20 @@ public class CarDao {
 		
 		int primaryKey = orm.save(car);
 		
+		if(primaryKey > 0) {
+			logger.info("A car object has successfully been inserted into the Car table with primary key " + primaryKey);
+		}
+		
 		return primaryKey;
 	}
 	
 	public boolean delete(int primaryKey) {
 		
 		boolean deleted = orm.remove("Car", primaryKey);
+		
+		if(deleted) {
+			logger.info("A record in the Car table with an id=" + primaryKey + " has successfully been deleted.");
+		}
 		
 		return deleted;
 		
@@ -35,6 +46,10 @@ public class CarDao {
 	public boolean update(Car car, int primaryKey) {
 		
 		boolean changed = orm.change("Car", car, primaryKey);
+		
+		if(changed) {
+			logger.info("A record in the Car table with id=" + primaryKey + " has successfully been updated.");
+		}
 		
 		return changed;
 				
@@ -45,6 +60,10 @@ public class CarDao {
 		
 		Car car = orm.select(primaryKey);
 		
+		if(!car.equals(null)) {
+			logger.info("A new record with id=" + primaryKey + " has successfully been selected.");
+		}
+		
 		return car;
 	}
 	
@@ -52,6 +71,10 @@ public class CarDao {
 	public ArrayList<Car> SelectAll() {
 		
 		ArrayList<Car> cars = orm.selectAll();
+		
+		if(cars.size() > 0) {
+			logger.info("All cars have successfully been selected from the Car table.");
+		}
 		
 		return cars;
 	}
--- src/main/java/com/revature/dao/ModelDao.java
+++ src/main/java/com/revature/dao/ModelDao.java
@@ -2,6 +2,8 @@ package com.revature.dao;
 
 import java.util.ArrayList;
 
+import org.apache.log4j.Logger;
+
 import com.revature.models.Car;
 import com.revature.models.Country;
 import com.revature.models.Manufacturer;
@@ -11,6 +13,8 @@ import com.revature.orm.ORM;
 public class ModelDao {
 	
 private ORM orm = new ORM();
+private static Logger logger = Logger.getLogger(ModelDao.class);
+
 	
 	public void initializeParentTables(ArrayList<Country> countries, ArrayList<Manufacturer> manufacturers) {
 		
@@ -21,12 +25,20 @@ private ORM orm = new ORM();
 		
 		int primaryKey = orm.save(model);
 		
+		if(primaryKey > 0) {
+			logger.info("A record in the Car table with an id=" + primaryKey + " has successfully been inserted.");
+		}
+		
 		return primaryKey;
 	}
 	
 	public boolean delete(int primaryKey) {
 		
 		boolean deleted = orm.remove("Model", primaryKey);
+		
+		if(deleted) {
+			logger.info("A record in the Car table with an id=" + primaryKey + " has successfully been deleted.");
+		}
 		
 		return deleted;
 		
@@ -35,6 +47,10 @@ private ORM orm = new ORM();
 	public boolean update(Model model, int primaryKey) {
 		
 		boolean changed = orm.change("Model", model, primaryKey);
+		
+		if(changed) {
+			logger.info("A record in the Car table with an id=" + primaryKey + " has successfully been updated.");
+		}
 		
 		return changed;	
 	
